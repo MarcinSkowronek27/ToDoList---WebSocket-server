ce6acc02dc599d8c61750df5cb5d0fa71f5640d2
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-17 20:11:52 +0200

    Prepare server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const socket = require('socket.io');
 
 const app = express();
-
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
+const io = socket(server);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
