c34f8d8a7333b76d2f1ad847e6d815a1d8c6a0ee
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-17 20:43:25 +0200

    Done server file for connection

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const server = app.listen(process.env.PORT || 8000, () => {
 });
 const io = socket(server);
 
+const tasks = [];
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (taskName) => {
+    tasks.push(taskName);
+    socket.broadcast.emit('addTask', tasks);
+  });
+  socket.on('removeTask', (removeId) => {
+    tasks.splice(tasks.indexOf(removeId, 1));
+    socket.broadcast.emit('removeTask', tasks);
+  });
+});
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
